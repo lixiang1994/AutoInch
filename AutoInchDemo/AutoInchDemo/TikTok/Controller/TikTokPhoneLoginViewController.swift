64664ba94d749d258a0443d1487c4b2099f6d1ff
lixiang1994 <18611401994@163.com>
2018-11-06 14:26:55 +0800

    完善登录页输入框相关细节

--- AutoInchDemo/AutoInchDemo/TikTok/Controller/TikTokPhoneLoginViewController.swift
+++ AutoInchDemo/AutoInchDemo/TikTok/Controller/TikTokPhoneLoginViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class TikTokPhoneLoginViewController: UIViewController {
 
     private lazy var layer = CAGradientLayer().then {
+        let colors: [CGColor] =  [#colorLiteral(red: 0.462745098, green: 0.4588235294, blue: 0.9607843137, alpha: 1), #colorLiteral(red: 0.5647058824, green: 0.2784313725, blue: 1, alpha: 1), #colorLiteral(red: 0.6588235294, green: 0.1803921569, blue: 0.9019607843, alpha: 1)]
         $0.locations = [0.0, 0.3, 1.0]
-        $0.colors = [#colorLiteral(red: 0.462745098, green: 0.4588235294, blue: 0.9607843137, alpha: 1).cgColor, #colorLiteral(red: 0.5647058824, green: 0.2784313725, blue: 1, alpha: 1).cgColor, #colorLiteral(red: 0.6588235294, green: 0.1803921569, blue: 0.9019607843, alpha: 1).cgColor]
+        $0.colors = colors
+        $0.opacity = 0.8
     }
     
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
@@ -22,6 +24,12 @@ class TikTokPhoneLoginViewController: UIViewController {
     
     @IBOutlet weak var areaButton: UIButton!
     @IBOutlet weak var captchaButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var otherView: UIView!
+    
+    private var phone: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +38,14 @@ class TikTokPhoneLoginViewController: UIViewController {
         setupNotification()
         
         view.layoutIfNeeded()
-        
+        phoneTextField.addTarget(self, action: #selector(phoneTextFieldAction), for: .editingChanged)
         phoneTextField.becomeFirstResponder()
+        
+        if let button = phoneTextField.value(forKey: "_clearButton") as? UIButton {
+            button.setImage(#imageLiteral(resourceName: "tiktok_textfield_clear"), for: .normal)
+            button.setImage(#imageLiteral(resourceName: "tiktok_textfield_clear"), for: .highlighted)
+        }
+        
     }
     
     override func viewDidLayoutSubviews() {
@@ -69,13 +83,48 @@ class TikTokPhoneLoginViewController: UIViewController {
     @IBAction func captchaAction(_ sender: UIButton) {
         
     }
+    
+    @IBAction func doneAction(_ sender: UIButton) {
+        
+    }
+    
+    @objc private func phoneTextFieldAction(_ sender: UITextField) {
+        guard let text = sender.text else {
+            return
+        }
+        
+        if text.count > phone.count {
+            if text.count == 3 || text.count == 8 {
+                sender.text = text + " "
+            }
+            if text.count >= 13 {
+                sender.text = String(text.prefix(13))
+                codeTextField.becomeFirstResponder()
+            }
+            
+            phone = text
+            
+        } else if text.count < phone.count {
+            if text.count == 3 || text.count == 8 {
+                sender.text = String(text.prefix(text.count - 1))
+            }
+            
+            phone = text
+        }
+        
+        UIView.beginAnimations("", context: nil)
+        UIView.setAnimationDuration(0.2)
+        captchaButton.isEnabled = !text.isEmpty
+        loginView.alpha = text.isEmpty ? 0 : 1
+        otherView.alpha = text.isEmpty ? 1 : 0
+        UIView.commitAnimations()
+    }
 }
 
 extension TikTokPhoneLoginViewController {
     
     private func setup() {
         view.layer.insertSublayer(layer, at: 0)
-        layer.opacity = 0.8
     }
     
     private func setupNotification() {
