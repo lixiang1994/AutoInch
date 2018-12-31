cae7939907c9b4a915de5f926e8995d304a3b77c
lixiang1994 <18611401994@163.com>
2018-12-31 18:10:26 +0800

    .

--- AutoInchDemo/AutoInchDemo/ViewController.swift
+++ AutoInchDemo/AutoInchDemo/ViewController.swift
@@ -17,9 +17,9 @@ class ViewController: UITableViewController {
         print("this is " +
             "default"
             .i35("3.5 inches (iPhone 4, 4s)")
-            .i40("3.5 inches (iPhone 5, 5s, SE)")
-            .i47("3.5 inches (iPhone 6, 7, 8)")
-            .i55("3.5 inches (iPhone 6, 7, 8 Plus)")
+            .i40("4.0 inches (iPhone 5, 5s, SE)")
+            .i47("4.7 inches (iPhone 6, 7, 8)")
+            .i55("5.5 inches (iPhone 6, 7, 8 Plus)")
             .ifull("full screen (iPhone X, Xs, XsMax)")
             .i58full("5.8 inches (iPhone X, Xs)")
             .i61full("6.1 inches (iPhone XR)")
