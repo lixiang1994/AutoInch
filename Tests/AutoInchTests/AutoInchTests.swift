d2f0b4162307039e44709258cdb0432273d9028d
LEE <18611401994@163.com>
2021-09-02 17:14:15 +0800

    添加SPM支持

---
+++ Tests/AutoInchTests/AutoInchTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import AutoInch
+
+    final class AutoInchTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(AutoInch().text, "Hello, World!")
+        }
+    }
